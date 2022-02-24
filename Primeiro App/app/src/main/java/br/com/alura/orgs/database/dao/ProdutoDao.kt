0e87dded9ca50f0d58ad62ff2e03bd79cb6df345
notprQn <90474257+notprQn@users.noreply.github.com>
2022-02-24 11:36:43 -0300

    Implementando Room

--- Primeiro App/app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
+++ Primeiro App/app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
@@ -1,9 +1,6 @@
 package br.com.alura.orgs.database.dao
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import br.com.alura.orgs.model.Produto
 
 @Dao
@@ -12,10 +9,13 @@ interface ProdutoDao {
     @Query("SELECT * FROM Produto")
     fun buscaTodos() : List<Produto>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun salva(vararg produto: Produto)
 
     @Delete
     fun remove(produto: Produto)
+
+    @Query("SELECT * FROM Produto WHERE id = :id")
+    fun buscaPorId(id: Long) : Produto?
 
 }
